3cb3b4d75246c23723b076c09ddbd01c8a1a2f93
Rayzler <cesar281103@gmail.com>
2024-05-08 18:50:47 -0600

    Fetching excluded

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 import { lusitana } from "@/app/ui/fonts";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import DashboardSkeleton, { LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 async function Dashboard() {
     return (
@@ -14,12 +13,12 @@ async function Dashboard() {
 
             </div>
             <div className={"mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"}>
-                <Suspense fallback={<RevenueChartSkeleton />}>
-                    <RevenueChart />
-                </Suspense>
-                <Suspense fallback={<LatestInvoicesSkeleton />}>
-                    <LatestInvoices />
-                </Suspense>
+                {/*<Suspense fallback={<RevenueChartSkeleton />}>*/}
+                {/*    <RevenueChart />*/}
+                {/*</Suspense>*/}
+                {/*<Suspense fallback={<LatestInvoicesSkeleton />}>*/}
+                {/*    <LatestInvoices />*/}
+                {/*</Suspense>*/}
             </div>
         </main>
     );
